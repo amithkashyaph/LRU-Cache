b39930e6b640c9a0f1d62ea812ac791a3f41e0a3
Amith Kashyap H <amithkashyaprv@gmail.com>
2026-01-28 19:22:53 +0530

    added node class

---
+++ src/LRU/Node.java
@@ -0,0 +1,25 @@
+package LRU;
+
+public class Node<K, V> {
+    K key;
+    V value;
+    Node<K, V> next;
+    Node<K, V> prev;
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
