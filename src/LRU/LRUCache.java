bc840431ced215bf53612b249d9d92e774064a2d
Amith Kashyap H <amithkashyaprv@gmail.com>
2026-01-30 20:20:37 +0530

    added print list method

--- src/LRU/LRUCache.java
+++ src/LRU/LRUCache.java
@@ -14,4 +14,36 @@ public class LRUCache<K, V> {
         this.linkedList = new DoublyLinkedList<>();
     }
 
+    public V get(K key) {
+        Node<K, V> node = map.get(key);
+        if(node == null) {
+            return null;
+        }
+
+        linkedList.removeAndMoveToFront(node);
+        return node.getValue();
+    }
+
+    public void put(K key, V value) {
+        Node<K, V> node = map.get(key);
+        if(node != null) {
+            node.setValue(value);
+            linkedList.removeAndMoveToFront(node);
+            return;
+        }
+        if(map.size() == initialCapacity) {
+            Node<K, V> removedNode = linkedList.removeLast();
+            map.remove(removedNode.getKey());
+        }
+
+        Node<K, V> newNode = new Node<>(key, value);
+        map.put(key, newNode);
+        linkedList.addFront(newNode);
+
+    }
+
+    public void printCache() {
+        linkedList.printList();
+    }
+
 }
